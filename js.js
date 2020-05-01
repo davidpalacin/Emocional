c960735298c9038b52fe257198ed1a89c634eb94
davidpalacin <55503430+davidpalacin@users.noreply.github.com>
2020-05-01 20:43:21 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,7 +1,27 @@
 $(document).ready(function Todo(){
+    $("#tablaJSON").hide();
+    $("#graficoJSON").hide();
+
     alert("Bienvenido a la página Proyecto Emocional");
     $(".celda").click(function(){
         alert($(this).data("x") + ", " + $(this).data("y"));
     });
+
+    $("#op1").click(function(){
+        MostrarTabla();
+    });
+
+    $("#op2").click(function(){
+        MostrarGrafico();
+    });
+
+    function MostrarTabla(){
+        $("#tablaJSON").show();
+        $("#graficoJSON").hide();
+    }
+    function MostrarGrafico(){
+        $("#graficoJSON").show();
+        $("#tablaJSON").hide();
+    }
 });
 
